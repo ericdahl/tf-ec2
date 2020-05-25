898899e63ef6984f66f0389920395142c3cfcc3f
Eric Dahl <eric.c.dahl@gmail.com>
2020-05-25 06:35:09 +0000

    add icmp

--- main.tf
+++ main.tf
@@ -45,4 +45,14 @@ resource "aws_security_group_rule" "allow_22" {
   cidr_blocks       = var.admin_cidrs
 }
 
+resource "aws_security_group_rule" "allow_icmp" {
+  security_group_id = aws_security_group.instance.id
+  type              = "ingress"
+  from_port         = -1 
+  to_port           = -1 
+  protocol          = "icmp"
+  cidr_blocks       = var.admin_cidrs
+}
+
+
 
